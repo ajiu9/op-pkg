4b78ab2a6b85c8a25c417ecbafbd1410990193f8
longbiao_wu <615944323@qq.com>
2024-12-27 12:56:48 +0800

    feat: add install pkg method

---
+++ src/install-pkg/detect.ts
@@ -0,0 +1,19 @@
+import type { Agent } from 'package-manager-detector'
+import process from 'node:process'
+import { detect } from 'package-manager-detector/detect'
+
+export type { Agent } from 'package-manager-detector'
+
+export type PackageManager = 'pnpm' | 'yarn' | 'npm' | 'bun'
+
+export async function detectPackageManager(cwd = process.cwd()): Promise<Agent | null> {
+  const result = await detect({
+    cwd,
+    onUnknown(packageManager) {
+      console.warn('[op-pkg] Unknown packageManager:', packageManager)
+      return undefined
+    },
+  })
+
+  return result?.agent || null
+}
--- test/source.test.ts
+++ test/source.test.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from 'node:fs'
 import { join } from 'node:path'
 import { expect, it } from 'vitest'
-import { getPackageInfo, importModule, isPackageExists, loadPackageJSON, resolveModule } from '../src'
+import { getPackageInfo, importModule, isPackageExists, loadPackageJSON, resolveModule } from '../src/local-pkg'
 
 it('test by source', async () => {
   expect(resolveModule('@ajiu9/eslint-config')).to.contain(join('node_modules', '@ajiu9', 'eslint-config'))
@@ -13,9 +13,11 @@ it('test by source', async () => {
   expect(isObject({})).to.eq(true)
 
   const info1 = await getPackageInfo('@ajiu9/eslint-config')
-  expect(!!info1).to.eq(true)
-  expect(info1.name).to.eq('@ajiu9/eslint-config')
-  expect(info1.packageJson.name).to.eq('@ajiu9/eslint-config')
+  if (info1) {
+    expect(!!info1).to.eq(true)
+    expect(info1.name).to.eq('@ajiu9/eslint-config')
+    expect(info1.packageJson.name).to.eq('@ajiu9/eslint-config')
+  }
 
-  expect(await loadPackageJSON()).to.eql(JSON.parse(await fs.readFile('./package.json'), 'utf-8'))
+  expect(await loadPackageJSON()).to.eql(JSON.parse(await fs.readFile('./package.json', 'utf-8')))
 })
